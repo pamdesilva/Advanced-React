666dacd74c7cf1a3696af3090ac5ac429c00d272
Pam De Silva <pam.desilva@gmail.com>
2022-07-29 17:07:13 +0100

    add skeleton files

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,12 @@
+import Document, { Html, Head, NextScript, Main } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    <Html lang="en-UK">
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>;
+  }
+}
