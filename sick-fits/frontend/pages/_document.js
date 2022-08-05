a01da52e480087521860b86340ef51347d9f33cf
Pam De Silva <pam.desilva@gmail.com>
2022-08-05 10:16:41 +0100

    wip

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -2,11 +2,13 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
-    <Html lang="en-UK">
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+    return (
+      <Html lang="en-UK">
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
